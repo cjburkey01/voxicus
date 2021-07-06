dfdd543f2f4a5ed23c642679953a702a8881dfed
CJ Burkey <cjburkey01@gmail.com>
2021-07-06 15:36:19 -0400

    Add passthrough features for metal/vulkan support

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,6 @@ use amethyst::renderer::{RenderToWindow, RenderingBundle};
 use amethyst::utils::application_root_dir;
 use amethyst::window::DisplayConfig;
 use amethyst::{Application, GameDataBuilder, SimpleState};
-use amethyst_imgui::RenderImgui;
-
-// The system that will execute with the IMGUI handle
-#[derive(Default, Clone, Copy)]
-struct ImguiSystem;
-impl<'a> amethyst::ecs::System<'a> for ImguiSystem {
-    type SystemData = ();
-
-    fn run(&mut self, _: Self::SystemData) {
-        amethyst_imgui::with(|ui| {
-            ui.show_demo_window(&mut true);
-        });
-    }
-}
 
 struct MainState;
 impl SimpleState for MainState {}
@@ -29,21 +15,18 @@ fn main() -> amethyst::Result<()> {
     let _app_root = application_root_dir()?;
 
     let game_data = GameDataBuilder::default()
-        // Include the IMGUI system
-        .with(ImguiSystem::default(), "imgui_use", &[])
         .with_bundle(InputBundle::<StringBindings>::default())?
         .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config(DisplayConfig {
-                        title: concat!(env!("CARGO_PKG_NAME"), " v", env!("CARGO_PKG_VERSION"))
-                            .to_owned(),
-                        maximized: true,
-                        ..Default::default()
-                    })
-                    .with_clear([0.34, 0.36, 0.52, 1.0]),
-                )
-                .with_plugin(RenderImgui::<StringBindings>::default()),
+            RenderingBundle::<DefaultBackend>::new().with_plugin(
+                RenderToWindow::from_config(DisplayConfig {
+                    title: concat!(env!("CARGO_PKG_NAME"), " v", env!("CARGO_PKG_VERSION"))
+                        .to_owned(),
+                    maximized: true,
+                    fullscreen: None,
+                    ..Default::default()
+                })
+                .with_clear([0.34, 0.36, 0.52, 1.0]),
+            ),
         )?;
 
     // Start the game
